2a6ef6a9418eed65bd03bbfb73ed63024b897e2f
J.K <K_940201@hotmail.com>
2018-08-14 11:57:48 -0700

    create button

--- App.js
+++ App.js
@@ -7,23 +7,31 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import {Platform, StyleSheet, Text, View, TouchableOpacity} from 'react-native';
 
 type Props = {};
 export default class App extends Component<Props> {
+  constructor(props){
+    super(props)
+    this.state = {
+      text: 'hi'
+    }
+  }
+
+  onPress = () => {
+    this.setState({
+      text: 'hi clicked'
+    })
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <TouchableOpacity onPress={this.onPress}>
+        <Text style={{backgroundColor:'yellow'}}>
+          {this.state.text}
+        </Text>
+        </TouchableOpacity>
       </View>
     );
   }
